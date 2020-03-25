1777001343a778de4c2842d1d85e6cc38db43100
Seth Todd <seth.todd@outlook.com>
2020-03-25 02:29:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+  
+  def initialize
+    @first_name = first_name
+    @last_name = last_name
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
 
 end
